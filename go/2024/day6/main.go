9199279013d2939d28fa0228afa545ca06173078
Simon Huang <simh@live.ca>
2025-02-24 10:06:24 -0500

    use vis set

--- go/2024/day6/main.go
+++ go/2024/day6/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
+	"sync/atomic"
 )
 
 func get[T any](v T, err error) T {
@@ -18,12 +20,14 @@ var directions = [][2]int{
 	{-1, 0}, {0, 1}, {1, 0}, {0, -1}, // Up -> right -> down -> left
 }
 
-func main() {
+func p2() {
 	if len(os.Args) != 2 {
 		log.Fatal("error with filepath")
 	}
 
 	f := get(os.Open(os.Args[1]))
+	defer f.Close()
+
 	r := bufio.NewScanner(f)
 
 	var graph [][]rune
@@ -32,24 +36,64 @@ func main() {
 		graph = append(graph, row)
 	}
 
-	vis := make(map[[2]int]bool)
+	var total int32
+	var wg sync.WaitGroup
+	sm := make(chan struct{}, 8) // 8 goroutines
 
 	for r := range graph {
 		for c := range graph[r] {
-			if graph[r][c] == '^' {
-				vis[[2]int{r, c}] = true
-				walk(graph, r, c, -1, 0, vis) //upwards
+			if graph[r][c] == '.' {
+				// do these in parallel
+				wg.Add(1)
+				sm <- struct{}{} // get the slot
+
+				go func(r, c int) {
+					defer wg.Done()
+					defer func() { <-sm }() //relesae slot
+					graphCopy := deepCopyGraph(graph)
+					graphCopy[r][c] = '#'
+					vis := make(map[[4]int]bool)
+					atomic.AddInt32(&total, int32(simulate(graphCopy, vis)))
+				}(r, c)
 			}
 		}
 	}
-	fmt.Printf("%v", len(vis))
+	wg.Wait()
+	fmt.Printf("%v", total)
 }
 
-func walk(graph [][]rune, r, c, dr, dc int, vis map[[2]int]bool) {
-	if r+dr < 0 || c+dc < 0 || r+dr >= len(graph) || c+dc >= len(graph[r]) {
-		return
+func deepCopyGraph(graph [][]rune) [][]rune {
+	copyGraph := make([][]rune, len(graph))
+	for i := range graph {
+		copyGraph[i] = append([]rune(nil), graph[i]...)
 	}
-	fmt.Printf("%v %v %v %v\n", r, c, dr, dc)
+	return copyGraph
+}
+
+func simulate(graph [][]rune, vis map[[4]int]bool) int {
+	for r := range graph {
+		for c := range graph[r] {
+			if graph[r][c] == '^' {
+				vis[[4]int{r, c, -1, 0}] = true
+				return walk(graph, r, c, -1, 0, vis, 0) //upwards
+			}
+		}
+	}
+	return 0
+}
+
+func walk(graph [][]rune, r, c, dr, dc int, vis map[[4]int]bool, turns int) int {
+	if r+dr < 0 || c+dc < 0 || r+dr >= len(graph) || c+dc >= len(graph[r]) {
+		return 0
+	}
+	if turns > 10000 {
+		return 1
+	}
+	// fmt.Printf("%v %v %v %v\n", r, c, dr, dc)
+	// hack to see if they are stuck in a loop
+	// if vis[[4]int{r + dr, c + dc, dr, dc}] {
+	// 	return 1
+	// }
 
 	if graph[r+dr][c+dc] == '#' {
 		//change dir
@@ -58,14 +102,11 @@ func walk(graph [][]rune, r, c, dr, dc int, vis map[[2]int]bool) {
 			if dr == dirs[0] && dc == dirs[1] {
 				newDr = directions[(i+1)%len(directions)][0] // circle back
 				newDc = directions[(i+1)%len(directions)][1] // circle back
-				fmt.Printf("newdr: %v %v\n", newDr, newDc)
 			}
 		}
-		vis[[2]int{r + newDr, c + newDc}] = true
-		walk(graph, r+newDr, c+newDc, newDr, newDc, vis)
-	} else {
-		vis[[2]int{r + dr, c + dc}] = true
-		walk(graph, r+dr, c+dc, dr, dc, vis)
+		vis[[4]int{r + newDr, c + newDc, newDr, newDc}] = true
+		return walk(graph, r+newDr, c+newDc, newDr, newDc, vis, turns+1)
 	}
-	return
+	vis[[4]int{r + dr, c + dc, dr, dc}] = true
+	return walk(graph, r+dr, c+dc, dr, dc, vis, turns+1)
 }
